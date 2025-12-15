56abdae15bf430b1135af38a959f51757f53bfda
norarubi510-cmd <norarubi510@gmail.com>
2025-12-15 15:51:48 -0500

    first commit

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ async function main() {
     console.log(
         usersData
         .map(
-            (user) =>  '<div class="user-card">
- <div class="user-card__container">
-    <h3>User's Name</h3>
-        <p><b>Email:</b> email@email.com</p>
-        <p><b>Phone:</b> 0000000000</p>
-        <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
- </div>
-</div>'      
+            (user) =>  <div class="user-card">
+    <div class="user-card__container">
+        <h3>User's Name</h4>
+            <p><b>Email:</b> email@email.com</p>
+            <p><b>Phone:</b> 0000000000</p>
+            <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+        </div>
+    </div>
         )
-        .join("")
-    );
-}
+       .join("")
+        );
 
+    }
 main();

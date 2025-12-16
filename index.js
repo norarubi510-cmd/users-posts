db8b9d9bfe9a0239bc97b1a4384d080dfa6090a4
norarubi510-cmd <norarubi510@gmail.com>
2025-12-16 09:07:01 -0500

    first commit

--- index.js
+++ index.js
@@ -3,22 +3,24 @@
 
 
 async function main() {
-    const users= await fetch ("https://jsonplaceholder.typicode.com/users");
-    const usersData= await users.json();
-    console.log(
-        usersData
-        .map(
-            (user) =>  <div class="user-card">
-    <div class="user-card__container">
-        <h3>User's Name</h4>
-            <p><b>Email:</b> email@email.com</p>
-            <p><b>Phone:</b> 0000000000</p>
-            <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+    const users = await fetch ("https://jsonplaceholder.typicode.com/users");
+    const userData = await users.json();
+    const userListEl = document.querySelector(".user-list");
+    
+    userListEl.innerHTML = userData
+      .map(
+        (user) => `<div  class="user-card">
+        <div class= "user-card_container">
+         <h3>${user.name}</h3>
+            <p><b>Email:</b>${user.email}</p>
+            <p><b>Phone:</b>${user.phone}</p>
+            <p><b>Website:</b> <a href= "${user.website}"
+            target="_blank"> ${user.website}
+            </a></p>
         </div>
-    </div>
-        )
-       .join("")
-        );
+    </div>`     
+    )
+    .join("");
+}
 
-    }
 main();
